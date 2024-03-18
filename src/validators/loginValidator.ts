55e7eb720b4e5d157b81dbff019805a0937f89ef
Zadorin Artyom <noardour@gmail.com>
2024-03-18 17:18:00 +0500

    fix login validation

--- src/validators/loginValidator.ts
+++ src/validators/loginValidator.ts
@@ -5,17 +5,17 @@ import z from "zod";
 
 const prisma = new PrismaClient();
 
-async function emailExist(email: string) {
-  const user = await prisma.user.findUnique({
-    where: { email },
+const loginSchema = z
+  .object({
+    email: z.string().min(1, "Email is required").email(),
+    password: z.string().min(4).max(30),
+  })
+  .superRefine(async (val, ctx) => {
+    const { email, password } = val;
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) ctx.addIssue({ code: z.ZodIssueCode.custom, message: "No such User", path: ["email"] });
+    if (user && user.password !== password) ctx.addIssue({ code: z.ZodIssueCode.custom, message: "Wrong password", path: ["password"] });
   });
-  return !!user;
-}
-
-const loginSchema = z.object({
-  email: z.string().min(1, "Email is required").email().refine(emailExist, "Wrong username."),
-  password: z.string().min(4).max(30),
-});
 
 export function validateLogin(formData: FormData) {
   return loginSchema.safeParseAsync({
