592d181b415132023e76dd8ee3bd4bf7e75e0cdf
Zadorin Artyom <noardour@gmail.com>
2024-03-19 00:34:30 +0500

    change seeder

--- src/components/auth/RegistrationForm.tsx
+++ src/components/auth/RegistrationForm.tsx
@@ -6,25 +6,29 @@ import { FC } from "react";
 import { useFormState } from "react-dom";
 
 const RegistrationForm: FC = () => {
-  const initialState: RegistrationState = {};
-  const [state, dispatch] = useFormState(register, initialState);
+  const [state, dispatch] = useFormState(register, undefined);
 
   return (
     <form action={dispatch} className="flex flex-col gap-4 xl:w-[75%]">
-      <Input label="Name" type="text" name="name" isInvalid={!!state.errors?.name} errorMessage={state.errors?.name?.join(" ")} />
-      <Input label="Email" type="text" name="email" isInvalid={!!state.errors?.email} errorMessage={state.errors?.email?.join(" ")} />
-      <Input label="Password" type="password" name="password" isInvalid={!!state.errors?.password} errorMessage={state.errors?.password?.join(" ")} />
+      <Input label="Name" type="text" name="name" isInvalid={!!state?.errors?.name} errorMessage={state?.errors?.name?.join(" ")} />
+      <Input label="Email" type="text" name="email" isInvalid={!!state?.errors?.email} errorMessage={state?.errors?.email?.join(" ")} />
+      <Input
+        label="Password"
+        type="password"
+        name="password"
+        isInvalid={!!state?.errors?.password}
+        errorMessage={state?.errors?.password?.join(" ")}
+      />
       <Input
         label="Confirm password"
         type="password"
         name="confirm-password"
-        isInvalid={!!state.errors?.confirmPassword}
-        errorMessage={state.errors?.confirmPassword?.join(" ")}
+        isInvalid={!!state?.errors?.confirmPassword}
+        errorMessage={state?.errors?.confirmPassword?.join(" ")}
       />
       <Button className="self-start" type="submit" color="primary">
         Register
       </Button>
-      {state?.msg && <div>{state.msg}</div>}
     </form>
   );
 };
