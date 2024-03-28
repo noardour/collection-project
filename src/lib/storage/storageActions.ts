6b3e17b60d5a94be749f4a60c4f3d1dafb5f49be
Zadorin Artyom <noardour@gmail.com>
2024-03-28 05:42:52 +0500

    asdfddsfs

--- src/lib/storage/storageActions.ts
+++ src/lib/storage/storageActions.ts
@@ -3,14 +3,15 @@ import { Storage } from "@google-cloud/storage";
 console.log(process.env.GCP_PROJECT_ID);
 console.log(process.env.GCP_PRIVATE_KEY);
 console.log(process.env.GOOGLE_PRIVATE_KEY);
+console.log(process.env.PRIVATE_KEY);
 
 export const getGCPCredentials = () => {
   // for Vercel, use environment variables
-  return process.env.GOOGLE_PRIVATE_KEY
+  return process.env.PRIVATE_KEY
     ? {
         credentials: {
           client_email: process.env.GCLOUD_SERVICE_ACCOUNT_EMAIL,
-          private_key: process.env.GOOGLE_PRIVATE_KEY,
+          private_key: process.env.PRIVATE_KEY,
         },
         projectId: process.env.GCP_PROJECT_ID,
       }
