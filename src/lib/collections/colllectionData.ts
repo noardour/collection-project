4f49c5a6b2c83f2af2742c969232bb3d474b7471
Zadorin Artyom <noardour@gmail.com>
2024-03-28 07:17:35 +0500

    add noStore to data

--- src/lib/collections/colllectionData.ts
+++ src/lib/collections/colllectionData.ts
@@ -1,8 +1,10 @@
 import { ICollection } from "@/types/ICollection";
 import { PrismaClient } from "@prisma/client";
+import { unstable_noStore } from "next/cache";
 
 const prisma = new PrismaClient();
 
 export async function fetchCollection(id: ICollection["id"]) {
+  unstable_noStore();
   return await prisma.collection.findUnique({ where: { id }, include: { items: true } });
 }
--- src/lib/users/userData.ts
+++ src/lib/users/userData.ts
@@ -2,14 +2,17 @@
 
 import { IUser } from "@/types/IUser";
 import { PrismaClient } from "@prisma/client";
+import { unstable_noStore } from "next/cache";
 
 const prisma = new PrismaClient();
 
 export async function fetchUsers(): Promise<IUser[]> {
+  unstable_noStore();
   return await prisma.user.findMany({ orderBy: [{ createdAt: "asc" }] });
 }
 
 export async function fetchUser(id: IUser["id"]) {
+  unstable_noStore();
   return await prisma.user.findUnique({
     where: { id },
     include: { collections: { include: { items: true } } },
