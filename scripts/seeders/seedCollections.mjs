d6dbdea5e9ef805b38698c245f122b8cb8d90c86
Zadorin Artyom <noardour@gmail.com>
2024-03-29 08:39:15 +0500

    change collection seeding

--- scripts/seeders/seedCollections.mjs
+++ scripts/seeders/seedCollections.mjs
@@ -3,28 +3,46 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const generateCollection = (id) => ({
+  image: faker.image.url({ height: 600, width: 800 }),
+  title: faker.lorem.word({ length: { min: 1, max: 5 } }),
+  category: "OTHER",
+  description: faker.lorem.paragraph({ min: 12, max: 36 }),
+  User: {
+    connect: { id },
+  },
+
+  items: {
+    create: Array.from({ length: 4 + Math.round(Math.random() * 6) }, () => ({
+      image: faker.image.url({ width: 400, height: 200 }),
+      title: faker.lorem.word({ length: { min: 1, max: 5 } }),
+    })),
+  },
+});
+
+/* const generateCarsCollection = (id) => ({
+  title: "Cars Collection",
+  category: "CARS",
+  description: faker.lorem.paragraph({ min: 12, max: 36 }),
+  userId: id,
+
+  items: {
+    create: Array.from({ length: 4 + Math.round(Math.random() * 6) }, () => ({
+      image: faker.image.url({ width: 400, height: 200 }),
+      title: faker.vehicle.vehicle(),
+    })),
+  },
+}); */
+
 const seedCollections = async () => {
   const users = await prisma.user.findMany({ where: { role: "USER" } });
+  const promises = [];
 
   users.forEach(async (user) => {
-    await prisma.collection.create({
-      data: {
-        title: "TestCollection",
-        category: "OTHER",
-        description:
-          "Имеется спорная точка зрения, гласящая примерно следующее: базовые сценарии поведения пользователей, превозмогая сложившуюся непростую экономическую ситуацию, указаны как претенденты на роль ключевых факторов. Равным образом, высокотехнологичная концепция общественного уклада влечет за собой процесс внедрения и модернизации системы массового участия. С другой стороны, выбранный нами инновационный путь способствует подготовке и реализации экспериментов, поражающих по своей масштабности и грандиозности.",
-        userId: user.id,
-
-        items: {
-          create: [
-            { image: faker.image.url({ width: 300, height: 200 }), title: "test item" },
-            { image: faker.image.url({ width: 300, height: 200 }), title: "test item 2" },
-            { image: faker.image.url({ width: 300, height: 200 }), title: "test item 3" },
-          ],
-        },
-      },
-    });
+    promises.push(prisma.collection.create({ data: generateCollection(user.id) }));
   });
+
+  await Promise.all(promises);
 };
 
 export default seedCollections;
